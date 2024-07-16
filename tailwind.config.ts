f610aef769ade37d181e738fc7e0feb6df6c2d46
msubham193 <msubham193@gmail.com>
2024-07-16 14:44:39 +0530

    done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        'custom-all': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 -4px 6px -1px rgba(0, 0, 0, 0.01), 4px 0 6px -1px rgba(0, 0, 0, 0.1), -4px 0 6px -1px rgba(0, 0, 0, 0.1)', // Shadow in all directions
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +18,23 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [function({ addUtilities }:any) {
+    addUtilities({
+      '.no-scrollbar': {
+        /* Hide scrollbar for Webkit browsers */
+        '-webkit-overflow-scrolling': 'touch',
+        'scrollbar-width': 'none',
+        '&::-webkit-scrollbar': {
+          display: 'none',
+        },
+      },
+      '.scrollbar-none': {
+        'scrollbar-width': 'none', /* Hide scrollbar for Firefox */
+        '&::-webkit-scrollbar': {
+          display: 'none', /* Hide scrollbar for Webkit browsers */
+        },
+      },
+    }, ['responsive']);
+  }],
 };
 export default config;
